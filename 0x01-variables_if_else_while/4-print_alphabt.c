597ab6236f784cb80fef75b1ff15d19a14b6aa36
Michelle <chiza.tadiwanashe@gmail.com>
2023-06-19 04:52:37 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: 0 (Success)
+*/
+int main(void)
+{
+char cha;
+for (cha = 'a'; cha <= 'z'; cha++)
+{
+if (cha != 'q' && cha != 'e')
+putchar(cha);
+}
+putchar('\n');
+return (0);
+}
