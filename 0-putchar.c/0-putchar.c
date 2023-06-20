5c21cf1a59722168ad801a4c10df9ea899848dea
Michelle <chiza.tadiwanashe@gmail.com>
2023-06-20 06:53:41 -0700

    0-putchar.c

--- 0-putchar.c/0-putchar.c
+++ 0-putchar.c/0-putchar.c
@@ -4,8 +4,24 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
-printf("_putchar\n");
+putchar('_');
+
+putchar('p');
+
+putchar('u');
+
+putchar('t');
+
+putchar('c');
+
+putchar('h');
+
+putchar('a');
+
+putchar('r');
+
+putchar('\n');
+
 return (0);
 }
